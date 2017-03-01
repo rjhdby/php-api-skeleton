8929f544add2cce609f48b76f411c583180de508
rjhdby <andrewgrom@rambler.ru>
2017-03-02 01:28:04 +0300

    test coverage

--- test/ControllerTest.php
+++ test/ControllerTest.php
@@ -5,7 +5,7 @@ use core\Controller;
 /** @noinspection LongInheritanceChainInspection */
 class ControllerTest extends PHPUnit_Framework_TestCase
 {
-    public function testExampleCall() {
+    public function testExampleCall () {
         $controller = new Controller(['m' => 'example']);
         $this->assertEquals(
             ['r' => ['mysql'], 'e' => []],
@@ -13,11 +13,34 @@ class ControllerTest extends PHPUnit_Framework_TestCase
         );
     }
 
-    public function testWrongCall() {
+    public function testWrongCall () {
         $controller = new Controller(['m' => 'nothing']);
         $this->assertEquals(
             ['r' => [], 'e' => ['code' => 0, 'text' => 'Wrong method']],
             $controller->run()
+        );
+    }
+
+    public function testUseStaticMapping () {
+        $method = new ReflectionMethod(
+            new Controller([]), 'mapStatic'
+        );
+        $method->setAccessible(true);
+        $this->assertEquals([
+                                'example'     => 'methods\Example',
+                                'wrongMethod' => 'methods\WrongMethod'
+                            ],
+                            $method->invoke(new Controller([]))
+        );
+    }
+
+    public function testParseTokensForDumbFile () {
+        $method = new ReflectionMethod(
+            new Controller([]), 'parseTokens'
+        );
+        $method->setAccessible(true);
+        $this->assertEquals(false,
+                            $method->invoke(new Controller([]), token_get_all(file_get_contents(ROOT . '/class/core/MethodInterface.php')))
         );
     }
 }

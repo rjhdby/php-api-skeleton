d52a2b8f346e5d93c6247f42b76e5ba8cc1c0f54
rjhdby <andrewgrom@rambler.ru>
2017-03-03 00:13:42 +0300

    environment constant CASE_SENSITIVE added

--- class/core/Controller.php
+++ class/core/Controller.php
@@ -1,5 +1,8 @@
 <?php
 namespace core;
+
+use methods\WrongMethod;
+
 /**
  * Class Controller
  * @package core
@@ -17,18 +20,21 @@ class Controller
      *
      * @param array $data
      */
-    public function __construct($data) {
+    public function __construct ($data) {
         $this->data    = $data;
         $this->methods = STATIC_MAPPING
             ? self::mapStatic()
             : self::mapDynamic();
+        if (!CASE_SENSITIVE) {
+            $this->methods = array_change_key_case($this->methods, CASE_LOWER);
+        }
     }
 
-    private static function mapStatic() {
+    private static function mapStatic () {
         return Config::parseCustomConfig(METHODS);
     }
 
-    private static function mapDynamic() {
+    private static function mapDynamic () {
         $methods = [];
         foreach (scandir(ROOT . '/class/methods') as $fileName) {
             if (substr($fileName, -4) !== '.php') {
@@ -36,26 +42,26 @@ class Controller
             }
             $current = self::parseTokens(token_get_all(file_get_contents(ROOT . '/class/methods/' . $fileName)));
             if ($current !== false) {
-                $methods[ key($current) ] = current($current);
+                $methods[key($current)] = current($current);
             }
         }
 
         return $methods;
     }
 
-    private static function parseTokens($tokens) {
+    private static function parseTokens ($tokens) {
         $namespace = '';
         $method    = '';
         for ($i = 0, $max = count($tokens); $i < $max; $i++) {
-            switch ($tokens[ $i ][0]) {
+            switch ($tokens[$i][0]) {
                 case T_CLASS:
-                    return $method ? [$method => $namespace . '\\' . $tokens[ $i + 2 ][1]] : false;
+                    return $method ? [$method => $namespace . '\\' . $tokens[$i + 2][1]] : false;
                 case T_NAMESPACE:
-                    $namespace = $tokens[ $i + 2 ][1];
+                    $namespace = $tokens[$i + 2][1];
                     break;
                 case T_DOC_COMMENT:
-                    if (preg_match('/@api-call/m', $tokens[ $i ][1]) !== 0) {
-                        $method = preg_replace("/.*@api-call\s+(\w+).*/s", "$1", $tokens[ $i ][1]);
+                    if (preg_match('/@api-call/m', $tokens[$i][1]) !== 0) {
+                        $method = preg_replace("/.*@api-call\s+(\w+).*/s", "$1", $tokens[$i][1]);
                     }
             }
         }
@@ -68,15 +74,20 @@ class Controller
      *
      * @return array
      */
-    public function run() {
-        $methodName = isset($this->data[ METHOD ])
-            ? $this->data[ METHOD ]
-            : 'wrongMethod';
-        $class      = isset($this->methods[ $methodName ])
-            ? $this->methods[ $methodName ]
-            : $this->methods['wrongMethod'];
-
+    public function run () {
         $result = ['r' => [], 'e' => []];
+        if (!isset($this->data[METHOD])) {
+            $result['e'] = ['code' => 0, 'text' => 'Unknown method'];
+            return $result;
+        }
+        $methodName = $this->data[METHOD];
+        if (CASE_SENSITIVE === false) {
+            $methodName = mb_strtolower($methodName);
+        }
+        $class = isset($this->methods[$methodName])
+            ? $this->methods[$methodName]
+            : WrongMethod::class;
+
         try {
             $request     = new $class($this->data);
             $result['r'] = $request();

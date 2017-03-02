2082b4c7a6e113afca7ef623edd997c1de2c0dad
Громов Андрей Олегович <AGromov@alfabank.ru>
2017-03-02 14:51:10 +0300

    Wron Method tune

--- class/methods/WrongMethod.php
+++ class/methods/WrongMethod.php
@@ -9,18 +9,21 @@ use core\MethodInterface;
 
 class WrongMethod implements MethodInterface
 {
+    private $text = '';
+
     /**
      * Method constructor.
      * @param array $data
      */
-    public function __construct ($data = null) {
+    public function __construct($data = null) {
+        $this->text = isset($data[ METHOD ]) ? 'Wrong method ' . $data[ METHOD ] : 'Unknown method';
     }
 
     /**
      * @return array
      * @throws \BadMethodCallException
      */
-    public function __invoke () {
-        throw new BadMethodCallException('Wrong method');
+    public function __invoke() {
+        throw new BadMethodCallException($this->text);
     }
 }
